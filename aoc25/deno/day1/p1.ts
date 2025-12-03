7d7dad956e1d143879c3513863de78938bec0a9b
Marcello Fabrizio <39314230+Marcellofabrizio@users.noreply.github.com>
2025-12-03 13:51:40 -0300

    Add files via upload

---
+++ aoc25/deno/day1/p1.ts
@@ -0,0 +1,45 @@
+const text = await Deno.readTextFile('input');
+
+const content = text.split('\n').map(line => line.trim()).filter(line => line.length > 0);
+
+const rotations = []
+
+content.forEach(line => {
+    const dir = line[0]
+    const dial = Number(line.slice(1))
+
+    rotations.push({
+        dir,
+        dial
+    })
+})
+
+
+var currentDial = 50
+var result = 0
+
+for (const rotation of rotations) {
+
+    let newDial
+
+    if (rotation.dir === "L") {
+        newDial = currentDial - rotation.dial
+    } else {
+        newDial = currentDial + rotation.dial
+    }
+
+    // console.log(rotation, currentDial, Math.floor((Math.abs(newDial)+100)/100))
+    // if (newDial < 0) {
+    //     result += Math.floor((Math.abs(newDial)+100)/100)
+    // } else if (newDial > 0) {
+    //     result += Math.floor(Math.abs(newDial)/100)
+    // }
+    //
+    currentDial = (newDial % 100 + 100) % 100
+
+    if (currentDial === 0) {
+        result++
+    }
+}
+
+console.log(result)
